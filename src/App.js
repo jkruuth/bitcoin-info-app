aa36aef402c387be1bc27a7f784e172c5b3e83a4
jkruuth <joonas.k.ruuth@student.jyu.fi>
2021-11-16 19:15:21 +0200

    User input type changed

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ const App = () => {
   const [data, setData] = useState([])
   const [fromDate, setFromDate] = useState('')
   const [toDate, setToDate] = useState('')
-  
+
   const handleSubmit = (event) => {
     event.preventDefault()
+
+    const oneHour = 3600
 
     const fromQuery = new Date(fromDate).getTime() / 1000
     const toQuery = new Date(toDate).getTime() / 1000
@@ -22,13 +24,13 @@ const App = () => {
     }
 
     const fetchData = async () => {
-      const res = await axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=${fromQuery}&to=${toQuery}`)
+      const res = await axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=${fromQuery}&to=${toQuery + oneHour}`)
 
       const { data } = res
+      
       setData(data)
-
       console.log(data)
-
+      
       console.log(fromQuery)
       console.log(toQuery)
       setFromDate('')
@@ -37,18 +39,28 @@ const App = () => {
     fetchData()
   }
 
+   
+
   return (
     <div className="App">
+      <h1>Bitcoin info app</h1>
+
+      <h2>Enter the desired time interval </h2>
+      
       <form onSubmit={handleSubmit}>
         <div>
           from
-          <input 
-            value={fromDate} 
+          <input
+            type="date"
+            value={fromDate}
+            placeholder="YYYY-MM-DD"
             onChange = {({ target }) => setFromDate(target.value)} 
           />
           to
           <input 
-            value={toDate} 
+            type="date"
+            value={toDate}
+            placeholder="YYYY-MM-DD"
             onChange = {({ target }) => setToDate(target.value)} 
           />
         </div>
