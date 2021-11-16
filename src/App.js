7c887b87da67a4133315a2ed653cb77082ed318c
jkruuth <joonas.k.ruuth@student.jyu.fi>
2021-11-16 17:50:59 +0200

    init and data fetching from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios"
 
-function App() {
+const App = () => {
+  
+
+  const [data, setData] = useState([])
+  const [fromDate, setFromDate] = useState('')
+  const [toDate, setToDate] = useState('')
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const fromQuery = new Date(fromDate).getTime() / 1000
+    const toQuery = new Date(toDate).getTime() / 1000
+
+    if (!fromDate || !toDate) {
+      
+      setFromDate('')
+      setToDate('')
+      return
+    }
+
+    const fetchData = async () => {
+      const res = await axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=${fromQuery}&to=${toQuery}`)
+
+      const { data } = res
+      setData(data)
+
+      console.log(data)
+
+      console.log(fromQuery)
+      console.log(toQuery)
+      setFromDate('')
+      setToDate('')
+    }
+    fetchData()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <div>
+          from
+          <input 
+            value={fromDate} 
+            onChange = {({ target }) => setFromDate(target.value)} 
+          />
+          to
+          <input 
+            value={toDate} 
+            onChange = {({ target }) => setToDate(target.value)} 
+          />
+        </div>
+        <button type='submit'>search</button>
+      </form>
     </div>
   );
 }
